c2d9a2780bf092da25c7c00791080e9a09a0ea79
Ken Harmon <harmkenn@gmail.com>
2022-07-13 18:08:29 +0000

    c01

--- Python-Course.eu/fibonacci.py
+++ Python-Course.eu/fibonacci.py
@@ -18,3 +18,11 @@ def fibi(n):
     for i in range(n-1):
         old, new = new, old + new
     return new
+
+memo = {0:0, 1:1}
+def fibm(n):
+    """ recursive Fibonacci function which memoizes previously 
+    calculated values with the help of a dictionary memo"""
+    if not n in memo:
+        memo[n] = fibm(n-1) + fibm(n-2)
+    return memo[n]
--- Python-Course.eu/fibonacci_runit.py
+++ Python-Course.eu/fibonacci_runit.py
@@ -1,12 +1,13 @@
 from timeit import Timer
+from fibonacci import fib
 
 t1 = Timer("fib(10)","from fibonacci import fib")
 
 for i in range(1, 20):
-    cmd = "fib(" + str(i) + ")"
-    t1 = Timer(cmd, "from fibonacci import fib")
+    s = "fibm(" + str(i) + ")"
+    t1 = Timer(s,"from fibonacci import fibm")
     time1 = t1.timeit(3)
-    cmd = "fibi(" + str(i) + ")"
-    t2 = Timer(cmd, "from fibonacci import fibi")
+    s = "fibi(" + str(i) + ")"
+    t2 = Timer(s,"from fibonacci import fibi")
     time2 = t2.timeit(3)
-    print(f"n={i:2d}, fib: {time1:9.7f}, fibi:  {time2:9.7f}, time1/time2: {time1/time2:10.2f}")
+    print(f"n={i:2d}, fibm: {time1:8.6f}, fibi:  {time2:7.6f}, time1/time2: {time1/time2:10.2f}")
