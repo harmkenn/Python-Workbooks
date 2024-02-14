20e2ec58654e2fda53f39ca0f83c44182f5f1c1f
Ken Harmon <harmkenn@gmail.com>
2024-02-14 08:05:43 +0100

    inline

--- Javascript_Bro_Code/BroCode07_Counter/index07.js
+++ Javascript_Bro_Code/BroCode07_Counter/index07.js
@@ -1,16 +1,19 @@
-// Constants https://www.youtube.com/watch?v=lfmg-EJ8gm4&list=WL&index=13&t=23655s
 
-const PI = 3.14159; 
-let radius;
-let circumference;
+const btnDecrease = document.getElementById("btnDecrease");
+const btnIncrease = document.getElementById("btnIncrease");
+const btnReset = document.getElementById("btnReset");
+const lblCount = document.getElementById("lblCount");
+let count = 0;
 
-
-
-
-
-document.getElementById("mySubmit").onclick = function(){
-    radius = Number(document.getElementById("myText").value);
-    circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = `Circumference is ${circumference}`;
+btnDecrease.onclick = function(){
+    count--;
+    lblCount.textContent = count;
 }
-
+btnIncrease.onclick = function(){
+    count++;
+    lblCount.textContent = count;
+}
+btnReset.onclick = function(){
+    count = 0;
+    lblCount.textContent = count;
+}
