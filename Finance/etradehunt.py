145b96d4e6e807bbc259a508a155c965adeb4920
Ken Harmon <41084577+harmkenn@users.noreply.github.com>
2024-08-15 10:24:01 +0000

    Update

--- Finance/etradehunt.py
+++ Finance/etradehunt.py
@@ -4,7 +4,7 @@ import plotly.graph_objs as go
 import numpy as np
 
 # List of index symbols
-index_symbols = ["^IXIC", "^GSPC", "^DJI", 'AMAGX','BPTRX','DXQLX','EILGX','FADTX','FKDNX','FSELX','FSHOX','FSPTX','JAGTX','LGRRX','NBGEX','PGTAX','PRDGX','RMQHX','ROGSX','SMPIX','SMPSX','UOPIX','VIHAX'
+index_symbols = ["^IXIC", "^GSPC", "^DJI", 'AMAGX','BPTRX','DXQLX','EILGX','FADTX','FKDNX','FSELX','FSHOX','FSPTX','JAGTX','LGRRX','PGTAX','PRDGX','RMQHX','ROGSX','SMPIX','SMPSX','UOPIX','GLD'
 ]
 
 # Initialize an empty figure
