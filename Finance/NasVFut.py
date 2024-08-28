76b40c2da7f800bc5689bc3ca2b951fcd187f31f
Ken Harmon <harmkenn@gmail.com>
2024-08-28 07:24:21 +0000

    Update

--- Finance/NasVFut.py
+++ Finance/NasVFut.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 # Define the time range for the last 30 days
 end_date = datetime.now()
-start_date = end_date - timedelta(days=30)
+start_date = end_date - timedelta(days=60)
 
 # Fetch data from Yahoo Finance
 @st.cache_data
