73a0297f28835e7ef5f931c41764b620610c942a
Ken Harmon <harmkenn@gmail.com>
2024-08-28 10:25:14 +0000

    Update

--- Finance/NASfromIndex.py
+++ Finance/NASfromIndex.py
@@ -73,7 +73,7 @@ def main():
     y_pred = model.predict(X)
 
     # Display the actual and predicted values
-    comparison = pd.DataFrame({'Actual ^IXIC % Change': y, 'Predicted ^IXIC % Change': y_pred})
+    comparison = pd.DataFrame({'^FTSE % Change':X['^FTSE % Change'], '^N225 % Change':X['^N225 % Change'], '^GDAXI % Change':X['^GDAXI % Change'],'Actual ^IXIC % Change': y, 'Predicted ^IXIC % Change': y_pred})
     st.write("Actual vs Predicted NASDAQ Percent Change:")
     st.dataframe(comparison)
 
@@ -88,6 +88,8 @@ def main():
     if st.button("Predict NASDAQ % Change"):
         today_prediction = model.predict([[ftse_today, nikkei_today, dax_today]])
         st.write(f"Predicted NASDAQ % Change for today: {today_prediction[0]:.5f}%")
+
+    
 
 if __name__ == "__main__":
     main()
