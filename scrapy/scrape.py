d0f1ce8bd5f2a9845b45e2f07b59142469b5c38e
Ken Harmon <harmkenn@gmail.com>
2024-09-06 12:26:15 +0000

    Update

--- scrapy/scrape.py
+++ scrapy/scrape.py
@@ -1,16 +1,23 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
-
+import undetected_chromedriver as uc
+import pandas as pd
+from selenium.webdriver.common.by import By
+import time
+import re
 
 
 
 def scrape_website(website):
   print('Launching chrome browser...')
 
-  chrome_driver_path = "./chromedriver.exe"
-  options = webdriver.ChromeOptions()
-  driver = webdriver.Chrome(service=Service(chrome_driver_path), options = options)
+  # Set up undetected-chromedriver
+  options = uc.ChromeOptions()
+  options.add_argument("--disable-blink-features=AutomationControlled")
+
+  # Create the WebDriver instance
+  driver = uc.Chrome(options=options)
   try:
     driver.get(website)
     print('Website loaded successfully')
