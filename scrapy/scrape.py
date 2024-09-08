ae51570620c875e68eccfbdb951f28ac07a123d9
Ken Harmon <harmkenn@gmail.com>
2024-09-08 17:34:16 +0200

    Update

--- scrapy/scrape.py
+++ scrapy/scrape.py
@@ -1,11 +1,6 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
-import undetected_chromedriver as uc
-import pandas as pd
-from selenium.webdriver.common.by import By
-import time
-import re
 
 def scrape_website(website):
   print('Launching chrome browser...')
@@ -14,8 +9,6 @@ def scrape_website(website):
   options = webdriver.ChromeOptions()
   driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
-  # Create the WebDriver instance
-  driver = uc.Chrome(options=options)
   try:
     driver.get(website)
     print('Website loaded successfully')
