52be8b719b6133f235dbcd9f865d2995c320c6f0
Ken Harmon <harmkenn@gmail.com>
2024-09-09 17:51:52 +0200

    Update

--- scrapy/AI_WebScraper.py
+++ scrapy/AI_WebScraper.py
@@ -4,12 +4,37 @@ import streamlit as st
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 import time
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 
+def extract_body_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    else:
+        return ""
+  
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, 'html.parser')
+    for script in soup(["script", "style"]):
+        script.extract()
+
+    cleaned_content = soup.get_text(separator="\n") 
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
+   
 
 st.title('AI Web Scraper')
-url = st.text_input('Enter URL: ', 'https://www.familysearch.org/')
-
-driver = None
+url = st.text_input('Enter URL: ', 'https://techwithtim.net')
 
 if 'driver' not in st.session_state:
     st.session_state.driver = None
@@ -30,12 +55,26 @@ if st.button('Continue') and st.session_state.loaded:
     st.session_state.driver.get(new_url)
     st.write('Scrapping...'  )
     html = st.session_state.driver.page_source
-    time.sleep(10)
-    st.write(html)
+    body_content = extract_body_content(html)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander('View DOM Content'):
+       st.text_area('DOM Content', cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area('Describe what you want to parse.')
+    if st.button('Parse Content'):
+        if parse_description:
+            st.write('Parsing the content...')
+            dom_chuncks = split_dom_content(st.session_state.dom_content)
+
 
 if st.button('Finished') and st.session_state.loaded:
     st.session_state.driver.quit()
   
+# https://youtu.be/Oo8-nEuDBkk?si=bUQr0kAADI17dD2n&t=1469
 
   
 
