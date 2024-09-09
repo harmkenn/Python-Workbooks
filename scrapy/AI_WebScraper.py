01d6a112015641edd4e3bf178ab65b687b5c4a69
Ken Harmon <harmkenn@gmail.com>
2024-09-09 16:57:53 +0200

    Update

--- scrapy/AI_WebScraper.py
+++ scrapy/AI_WebScraper.py
@@ -1,15 +1,46 @@
 # https://www.youtube.com/watch?v=Oo8-nEuDBkk&list=PLr51Y6xnCThP7MFXPplDCxWGBCvdQKmE8&index=16
 
 import streamlit as st
-from scrape import scrape_website
+import undetected_chromedriver as uc
+from bs4 import BeautifulSoup
+import time
+
 
 st.title('AI Web Scraper')
-url = st.text_input('Enter URL: ', 'https://www.bing.com/')
+url = st.text_input('Enter URL: ', 'https://www.familysearch.org/')
 
-st.button("Click here after logging in")
+driver = None
 
-if st.button('Scrape'):
-  st.button("Click here after logging in")
-  st.write('Scrapping...'  )
-  result = scrape_website(url)
-  st.write(result)
+if 'driver' not in st.session_state:
+    st.session_state.driver = None
+    st.session_state.loaded = False
+
+if st.button('Load Initial Page'):
+    print('Launching chrome browser...')
+    options = uc.ChromeOptions()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    st.session_state.driver = uc.Chrome(options=options)
+    st.session_state.driver.get(url)
+    st.write('Page loaded. Please navigate to the desired page.')
+    st.warning('Click the "Continue" button when you are finished navigating.')
+    st.session_state.loaded = True
+
+if st.button('Continue') and st.session_state.loaded:
+    new_url = st.session_state.driver.current_url
+    st.session_state.driver.get(new_url)
+    st.write('Scrapping...'  )
+    html = st.session_state.driver.page_source
+    time.sleep(10)
+    st.write(html)
+
+if st.button('Finished') and st.session_state.loaded:
+    st.session_state.driver.quit()
+  
+
+  
+
+
+
+  
+
+  
--- scrapy/scrape.py
+++ scrapy/scrape.py
@@ -1,33 +1,5 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import undetected_chromedriver as uc
-from bs4 import BeautifulSoup
-import time
 
-def scrape_website(website):
-  print('Launching chrome browser...')
 
-  options = uc.ChromeOptions()
-  options.add_argument("--disable-blink-features=AutomationControlled")
-  #options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"    # Replace with actual path
-  driver = uc.Chrome(options=options)
-
-  try:
-    driver.get(website)
-    print('Website loaded successfully')
-    html = driver.page_source
-    time.sleep(10)
-    
-    return html
-  finally:
-    driver.quit()
-
-def extract_body_content(html_content):
-  soup = BeautifulSoup(html_content, 'html.parser')
-  body_content = soup.body
-  if body_content:
-    return str(body_content)
-  return "Body content not found."
 
 # https://youtu.be/Oo8-nEuDBkk?si=bUQr0kAADI17dD2n&t=1469
     
