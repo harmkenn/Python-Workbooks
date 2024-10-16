6e7bf698c8c8c189ecc498651a8a7956aaf839c0
Ken Harmon <harmkenn@gmail.com>
2024-10-16 10:29:58 +0000

    Update

--- Finance/TQQQplan.py
+++ Finance/TQQQplan.py
@@ -63,11 +63,9 @@ for i in range(1, len(tqqq_data)):
              tqqq_data.columns.get_loc('Close')])/tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Close')]
         chunks = floor(tqqq_data.iloc[i, tqqq_data.columns.get_loc('Raise')]/inc)
         tqqq_data.iloc[i, tqqq_data.columns.get_loc('chunks')] = chunks
-        while tqqq_data.iloc[i, tqqq_data.columns.get_loc('High')] > 100:
+        for i in chunks:
             
-        # Sell first chunk of the shares
-        
-            tqqq_data.iloc[i, tqqq_data.columns.get_loc('Buy/Sell')] = 'Sell'
+            tqqq_data.iloc[i, tqqq_data.columns.get_loc('Buy/Sell')] = 'Sell'+str(i)
             shares_to_sell = int(tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Shares')] * chunk)
             cash_to_receive = shares_to_sell * (tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Close')] * (1+inc))
             tqqq_data.iloc[i, tqqq_data.columns.get_loc('Shares')] = tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Shares')] - shares_to_sell
