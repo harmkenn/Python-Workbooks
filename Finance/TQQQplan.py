f6a9ac14c69258a7fded80fbd3c559af8c26a174
Ken Harmon <harmkenn@gmail.com>
2024-10-16 10:49:00 +0000

    Update

--- Finance/TQQQplan.py
+++ Finance/TQQQplan.py
@@ -63,12 +63,14 @@ for i in range(1, len(tqqq_data)):
              tqqq_data.columns.get_loc('Close')])/tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Close')]
         chunks = floor(tqqq_data.iloc[i, tqqq_data.columns.get_loc('Raise')]/inc)
         tqqq_data.iloc[i, tqqq_data.columns.get_loc('chunks')] = chunks
+        shares_left = tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Shares')]
         j = 1
         while j <= chunks:
             tqqq_data.iloc[i, tqqq_data.columns.get_loc('Buy/Sell')] = 'Sell'+str(j)
-            shares_to_sell = int(tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Shares')] * chunk)
+            shares_to_sell = int(shares_left * chunk)
+            shares_left = shares_left - shares_to_sell
+            tqqq_data.iloc[i, tqqq_data.columns.get_loc('Shares')] = shares_left
             cash_to_receive = shares_to_sell * (tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Close')] * (1+inc))
-            tqqq_data.iloc[i, tqqq_data.columns.get_loc('Shares')] = tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Shares')] - shares_to_sell
             tqqq_data.iloc[i, tqqq_data.columns.get_loc('Cash')] = tqqq_data.iloc[i-1, tqqq_data.columns.get_loc('Cash')] + cash_to_receive
             j += 1
 
