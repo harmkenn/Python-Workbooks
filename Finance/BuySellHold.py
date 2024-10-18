350588a95b5842f1ae0cf74803b480e1cd379f18
Ken Harmon <harmkenn@gmail.com>
2024-10-18 10:16:31 +0000

    Update to just 3 and 3

--- Finance/BuySellHold.py
+++ Finance/BuySellHold.py
@@ -13,13 +13,14 @@ st.title("Buy Sell Hold Strategy")
 # Set the ticker symbol
 c1,c2 = st.columns([1,4])
 with c1:
-    ticker = st.selectbox("Select Ticker", ["TQQQ", "SOXL", "SMH", "DGRO" , "VIG"])
+    ticker = st.selectbox("Select Ticker", ["TQQQ", "SOXL", "DGRO"])
 
 # Fetch historical data for the past year
 data = yf.download(ticker, period='5y', interval='1d')
 
 # Calculate moving averages
 data['50_MA'] = data['Close'].rolling(window=50).mean()
+data['100_MA'] = data['Close'].rolling(window=100).mean()
 data['200_MA'] = data['Close'].rolling(window=200).mean()
 
 # Calculate annual returns
@@ -42,6 +43,7 @@ fig.add_trace(go.Ohlc(x=data.index,
 
 # Add moving averages
 fig.add_trace(go.Scatter(x=data.index, y=data['50_MA'], mode='lines', name='50-Day MA', line=dict(color='blue', width=1)))
+fig.add_trace(go.Scatter(x=data.index, y=data['100_MA'], mode='lines', name='100-Day MA', line=dict(color='purple', width=1)))
 fig.add_trace(go.Scatter(x=data.index, y=data['200_MA'], mode='lines', name='200-Day MA', line=dict(color='red', width=1)))
 
 # Update layout
