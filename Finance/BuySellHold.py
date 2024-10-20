4b115a7722f17bf7fde8a98cc1672af88be1a7d8
Ken Harmon <harmkenn@gmail.com>
2024-10-20 08:29:55 +0200

    Update

--- Finance/BuySellHold.py
+++ Finance/BuySellHold.py
@@ -3,6 +3,7 @@ import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
 from numpy import sqrt, maximum, minimum
+import datetime as dt
 
 # Set the page layout to wide
 st.set_page_config(layout="wide", page_title=f"Buy Sell Hold Strategy")
@@ -16,9 +17,13 @@ with c1:
     ticker = st.selectbox("Select Ticker", ['DGRO','EDEN','GBTC','INCO','KBWP','SMIN','SOXL','SPXL','SSO','TECL','TQQQ','UPRO'])
 with c2:
     bound = st.number_input("Bound", min_value=0.0, max_value=10.0, value=1.1, step=0.1)
+with c4:
+    start_date = st.date_input("Select start date", value=dt.date(2020, 1, 1), min_value=dt.date(2010, 1, 1), max_value=dt.date.today())  # replace with your desired start date
+    end_date = st.date_input("Select end date", value=dt.date.today(), min_value=dt.date(2010, 1, 1), max_value=dt.date.today())  # replace with your desired start date
 
 # Fetch historical data for the past year
-data = yf.download(ticker, period='2y', interval='1d')
+data = yf.download(ticker, start=start_date, end=end_date, interval='1d')
+data = data.drop(['Volume', 'Adj Close'], axis=1)
 
 # Calculate moving averages
 data['50_MA'] = data['Close'].rolling(window=50).mean()
@@ -56,20 +61,19 @@ def calculate_mfi(data, window=14):
 
 data = calculate_mfi(data)
 
+# Display final RSI and MFI
+with c1:
+    st.write(f"Final RSI (30,70) for {ticker}: {data['RSI'].iloc[-1]:.2f}")
+    st.write(f"Final MFI (20,80) for {ticker}: {data['MFI'].iloc[-1]:.2f}")
+
 # Display Standard Deviation and Buy/Sell levels
 with c2:
     st.write(f"Standard Deviation for {ticker}: {std_dev:.2f}%")
 with c3:
     st.write(f'Sell at: {data["UB"].iloc[-1]:.2f}')
     st.write(f'High: {data["High"].iloc[-1]:.2f}')
-with c4:
-    st.write(f'Buy at: {data["LB"].iloc[-1]:.2f}')
     st.write(f'Low: {data["Low"].iloc[-1]:.2f}')
-
-# Display final RSI and MFI
-with c1:
-    st.write(f"Final RSI (30,70) for {ticker}: {data['RSI'].iloc[-1]:.2f}")
-    st.write(f"Final MFI (20,80) for {ticker}: {data['MFI'].iloc[-1]:.2f}")
+    st.write(f'Buy at: {data["LB"].iloc[-1]:.2f}')
 
 # Drop the first 200 rows
 data = data.iloc[200:].copy()
